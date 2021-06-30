c85f5a56051f1a4a26a058deea9455faabef4264
Emre Can <emrecanyalcin1996@gmail.com>
2021-06-30 23:40:24 +0300

    Initial commit

---
+++ src/main/java/com/example/sonarclouddemo/SonarclouddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarclouddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarclouddemoApplication.class, args);
+	}
+
+}
